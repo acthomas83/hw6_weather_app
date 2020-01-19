7e881829d4a9e555f4385e8aa11a3fb56bcf18a9
Aaron Thomas <acthomas83@gmail.com>
2020-01-19 00:19:46 -0500

    Cities saved in localStorage. API linked via AJAX.

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
 var dateTime = document.getElementById("#date-time");
-var time = moment().format('h:mm:ss a');
-var date = moment().format('MMMM Do YYYY');
-var day = moment().format('dddd');
+var time = moment().format("h:mm:ss a");
+var date = moment().format("MMMM Do YYYY");
+var day = moment().format("dddd");
 console.log(time);
 console.log(date);
 console.log(day);
 
-var userSelection = document.getElementById('#user-input');
-var searchBtn = document.getElementById("search-btn")
-
-var div = document.getElementById("time");
-div.innerHTML += day + ", " + date;
-
-
-
+var userSelection = document.getElementById("#user-input");
+var searchBtn = document.getElementById("search-btn");
 
 $("#search-btn").on("click", function() {
-    var currentCity = $("input").val();
+  var currentCity = $("input").val();
   
-    console.log(currentCity);
-  
-    //localStorage.setItem($(this).attr("data-hour"), event);
-    localStorage.setItem(currentCity);
-    console.log(localStorage.setItem(currentCity));
-  
-    // add attributes to the save buttons
+
+  console.log(currentCity);
+
+  localStorage.setItem("City", currentCity);
+  console.log(localStorage.setItem("City", currentCity));
+
+  var div1 = document.getElementById("current-weather");
+  div1.innerHTML += "Hello, " + currentCity + "!";
+  // add attributes to the save buttons
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    currentCity +
+    "&appid=4b0342f3707425c92eb021e9050a8450";
+  // });
+
+  var div2 = document.getElementById("time");
+  div2.innerHTML += "Today is " + day + ", " + date;
+  // div.innerHTML += "Hello, " + currentCity;
+
+  // Creates AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var conditionsIcon = $(response.weather[0].icon);
+    $("#weather-icon").append(conditionsIcon);
+    console.log(conditionsIcon);
+
+
+
+    // $(".movie-data").text(JSON.stringify(response.Rated));
+    // Retrieves the release year
+
+    // Creates an element to hold the release year
+    // $(".movie-data").text(JSON.stringify(response.Released));
   });
+});
